fe304d906e10a6b7cdd7596e0aea44db76ded2de
Oleksandr-bod <karoska2@ukr.net>
2025-10-13 22:08:37 +0300

    Add files via upload

---
+++ data/text.js
@@ -0,0 +1,43 @@
+const BOOK_TEXT = [
+    { en: "HUCKLEBERRY FINN", uk: "ГАКЛБЕРІ ФІНН"},
+    { en: "CHAPTER I", uk: "РОЗДІЛ I"},
+    { en: "You don’t know about me without you have read a book by the name of The Adventures of Tom Sawyer; but that ain’t no matter.", uk: "Ви не знаєте про мене, якщо не читали книжки під назвою «Пригоди Тома Сойєра», але це не має значення."},
+    { en: "That book was made by Mr. Mark Twain, and he told the truth, mainly.", uk: "Цю книгу написав пан Марк Твен, і він, здебільшого, сказав правду."},
+    { en: "There was things which he stretched, but mainly he told the truth.", uk: "Були речі, які він натягував, але здебільшого він говорив правду."},
+    { en: "That is nothing.", uk: "Це нічого."},
+    { en: "I never seen anybody but lied one time or another, without it was Aunt Polly, or the widow, or maybe Mary.", uk: "Я ніколи не бачив нікого, хто б не брехав раз чи раз, хіба що тітка Поллі, чи вдова, чи, можливо, Мері."},
+    { en: "Aunt Polly—Tom’s Aunt Polly, she is—and Mary, and the Widow Douglas is all told about in that book, which is mostly a true book, with some stretchers, as I said before.", uk: "Тітка Поллі — вона ж тітка Поллі Тома — і Мері, і вдова Дуглас — про всіх розповідається в цій книзі, яка здебільшого є правдивою книгою, з деякими фрагментами, як я вже казав."},
+    { en: "Now the way that the book winds up is this: Tom and me found the money that the robbers hid in the cave, and it made us rich.", uk: "А ось як закінчується книга: ми з Томом знайшли гроші, які розбійники сховали в печері, і це збагатило нас."},
+    { en: "We got six thousand dollars apiece—all gold.", uk: "Ми отримали по шість тисяч доларів кожен — все золото."},
+    { en: "It was an awful sight of money when it was piled up.", uk: "Це було жахливе видовище, коли гроші були складені купою."},
+    { en: "Well, Judge Thatcher he took it and put it out at interest, and it fetched us a dollar a day apiece all the year round—more than a body could tell what to do with.", uk: "Ну, суддя Тетчер взяв його та виставив на продаж під відсотки, і це приносило нам по долару на день кожен цілий рік — більше, ніж кожен міг би сказати, куди з цим подіти."},
+    { en: "The Widow Douglas she took me for her son, and allowed she would sivilize me; but it was rough living in the house all the time, considering how dismal regular and decent the widow was in all her ways; and so when I couldn’t stand it no longer I lit out.", uk: "Вдова Дуглас прийняла мене за свого сина і дозволила собі поводитися зі мною цивілізовано; але було важко постійно жити в будинку, враховуючи, якою похмурою, правильною та порядною була ця вдова в усіх своїх звичках; тому, коли я більше не міг цього терпіти, я вибіг."},
+    { en: "I got into my old rags and my sugar-hogshead again, and was free and satisfied. But Tom Sawyer he hunted me up and said he was going to start a band of robbers, and I might join if I would go back to the widow and be respectable.", uk: "Я знову вліз у своє старе лахміття та свою цукрову бочку, і був вільний і задоволений. Але Том Сойєр розшукав мене і сказав, що збирається створити банду розбійників, і я міг би приєднатися до неї, якщо повернуся до вдови та буду порядним."},
+    { en: "So I went back.", uk: "Тож я повернувся назад."},
+]
+
+
+
+
+
+
+
+
+
+// const BOOK_TEXT = [
+//   { en: "You don’t know about me without you have read a book by the name of The Adventures of Tom Sawyer.", uk: "Ви не знаєте про мене, якщо не читали книгу під назвою «Пригоди Тома Сойєра»." },
+//   { en: "That book was made by Mr. Mark Twain.", uk: "Ту книгу написав містер Марк Твен." },
+//   { en: "He told the truth, mainly.", uk: "Він здебільшого говорив правду." },
+//   { en: "Some things he stretched, but mainly he told the truth.", uk: "Деякі речі він перебільшував, але в основному говорив правду." },
+//   { en: "There was things which he stretched, but I don't mind that.", uk: "Були речі, які він прикрашав, але я не проти цього." },
+//   { en: "Now the way that the book winds up is this: Tom and me found the money.", uk: "А в кінці книги Том і я знайшли гроші." },
+//   { en: "We got six thousand dollars apiece — all gold.", uk: "Ми отримали по шість тисяч доларів кожен — усе в золоті." },
+//   { en: "It was a lot of money for those times.", uk: "Це були великі гроші на ті часи." },
+//   { en: "Judge Thatcher took it and put it out at interest.", uk: "Суддя Тетчер узяв ці гроші й пустив їх під відсотки." },
+//   { en: "It fetched us a dollar a day apiece all the year round.", uk: "Це приносило нам долар на день кожному протягом року." },
+//   { en: "The Widow Douglas took me for her son, and allowed she would sivilize me.", uk: "Вдова Дуглас взяла мене до себе, щоб «цивілізувати»." },
+//   { en: "But it was rough living in the house all the time.", uk: "Але постійно жити в домі було нелегко." },
+//   { en: "So when I couldn’t stand it no longer, I lit out.", uk: "Тому коли я більше не витримав, я втік." },
+//   { en: "Tom Sawyer he hunted me up and said he was going to start a band of robbers.", uk: "Том Сойєр знайшов мене і сказав, що збирається створити банду розбійників." },
+//   { en: "I was to be one of them.", uk: "Я мав бути одним з них." }
+// ];
