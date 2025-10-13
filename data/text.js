d1b1ab611b38d6112ae264d35a318cca04a371d3
Oleksandr-bod <karoska2@ukr.net>
2025-10-13 22:35:30 +0300

    Update text.js

--- data/text.js
+++ data/text.js
@@ -14,6 +14,19 @@ const BOOK_TEXT = [
     { en: "The Widow Douglas she took me for her son, and allowed she would sivilize me; but it was rough living in the house all the time, considering how dismal regular and decent the widow was in all her ways; and so when I couldn’t stand it no longer I lit out.", uk: "Вдова Дуглас прийняла мене за свого сина і дозволила собі поводитися зі мною цивілізовано; але було важко постійно жити в будинку, враховуючи, якою похмурою, правильною та порядною була ця вдова в усіх своїх звичках; тому, коли я більше не міг цього терпіти, я вибіг."},
     { en: "I got into my old rags and my sugar-hogshead again, and was free and satisfied. But Tom Sawyer he hunted me up and said he was going to start a band of robbers, and I might join if I would go back to the widow and be respectable.", uk: "Я знову вліз у своє старе лахміття та свою цукрову бочку, і був вільний і задоволений. Але Том Сойєр розшукав мене і сказав, що збирається створити банду розбійників, і я міг би приєднатися до неї, якщо повернуся до вдови та буду порядним."},
     { en: "So I went back.", uk: "Тож я повернувся назад."},
+    //-------------------
+    { en: "The widow she cried over me, and called me a poor lost lamb, and she called me a lot of other names, too, but she never meant no harm by it.", uk: "Вдова плакала надо мною і називала мене бідним загубленим ягням, і ще багатьма іншими словами, але вона ніколи не хотіла цим образити мене."},
+    { en: "She put me in them new clothes again, and I couldn’t do nothing but sweat and sweat, and feel all cramped up. ", uk: "Вона знову одягла на мене той новий одяг, і я нічого не міг робити, окрім як пітніти, пітніти та відчувати себе вся в судомах."},
+    { en: "Well, then, the old thing commenced again.", uk: "Ну, тоді знову почалося старе."},
+    { en: "The widow rung a bell for supper, and you had to come to time.", uk: "Вдова подзвонила у дзвінок, щоб попросити вечерю, і ти мав прийти вчасно."},
+    { en: "When you got to the table you couldn’t go right to eating, but you had to wait for the widow to tuck down her head and grumble a little over the victuals, though there warn’t really anything the matter with them—that is, nothing only everything was cooked by itself.", uk: "Коли ти сідав за стіл, то не міг одразу почати їсти, а мав чекати, поки вдова опустить голову і трохи побурчить на їжу, хоча насправді з нею нічого не було не так — тобто, нічого, просто все готувалось самостійно."},
+    { en: "In a barrel of odds and ends it is different; things get mixed up, and the juice kind of swaps around, and the things go better.", uk: "У бочці з всякими дрібницями все інакше: речі змішуються, сік ніби міняється місцями, і справи йдуть краще."},
+    // { en: "", uk: ""},
+    // { en: "", uk: ""},
+    // { en: "", uk: ""},
+    // { en: "", uk: ""},
+    // { en: "", uk: ""},{ en: "", uk: ""},
+
 ]
 
 
@@ -24,20 +37,3 @@ const BOOK_TEXT = [
 
 
 
-// const BOOK_TEXT = [
-//   { en: "You don’t know about me without you have read a book by the name of The Adventures of Tom Sawyer.", uk: "Ви не знаєте про мене, якщо не читали книгу під назвою «Пригоди Тома Сойєра»." },
-//   { en: "That book was made by Mr. Mark Twain.", uk: "Ту книгу написав містер Марк Твен." },
-//   { en: "He told the truth, mainly.", uk: "Він здебільшого говорив правду." },
-//   { en: "Some things he stretched, but mainly he told the truth.", uk: "Деякі речі він перебільшував, але в основному говорив правду." },
-//   { en: "There was things which he stretched, but I don't mind that.", uk: "Були речі, які він прикрашав, але я не проти цього." },
-//   { en: "Now the way that the book winds up is this: Tom and me found the money.", uk: "А в кінці книги Том і я знайшли гроші." },
-//   { en: "We got six thousand dollars apiece — all gold.", uk: "Ми отримали по шість тисяч доларів кожен — усе в золоті." },
-//   { en: "It was a lot of money for those times.", uk: "Це були великі гроші на ті часи." },
-//   { en: "Judge Thatcher took it and put it out at interest.", uk: "Суддя Тетчер узяв ці гроші й пустив їх під відсотки." },
-//   { en: "It fetched us a dollar a day apiece all the year round.", uk: "Це приносило нам долар на день кожному протягом року." },
-//   { en: "The Widow Douglas took me for her son, and allowed she would sivilize me.", uk: "Вдова Дуглас взяла мене до себе, щоб «цивілізувати»." },
-//   { en: "But it was rough living in the house all the time.", uk: "Але постійно жити в домі було нелегко." },
-//   { en: "So when I couldn’t stand it no longer, I lit out.", uk: "Тому коли я більше не витримав, я втік." },
-//   { en: "Tom Sawyer he hunted me up and said he was going to start a band of robbers.", uk: "Том Сойєр знайшов мене і сказав, що збирається створити банду розбійників." },
-//   { en: "I was to be one of them.", uk: "Я мав бути одним з них." }
-// ];
